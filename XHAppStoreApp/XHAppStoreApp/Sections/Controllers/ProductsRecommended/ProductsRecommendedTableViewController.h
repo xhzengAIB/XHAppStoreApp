bcc2b056e880cba7efded5736398b8402cd8f332
Jack <xhzengAIB@gmail.com>
2014-11-21 12:31:47 +0800

    Add new project

---
+++ XHAppStoreApp/XHAppStoreApp/Sections/Controllers/ProductsRecommended/ProductsRecommendedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductsRecommendedTableViewController.h
+//  XHAppStoreApp
+//
+//  Created by dw_iOS on 14-11-21.
+//  Copyright (c) 2014年 广州华多网络科技有限公司 多玩事业部 iOS软件工程师 曾宪华 QQ：543413507. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductsRecommendedTableViewController : UITableViewController
+
+@end
